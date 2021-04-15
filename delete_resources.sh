36a24863ef5401b3dcf9a31864e310d02b031502
Baichuan Sun <baichsun@amazon.com>
2021-04-15 15:55:51 +1000

    🐛 delete cfn stack name

--- delete_resources.sh
+++ delete_resources.sh
@@ -12,7 +12,7 @@ echo "deleting ${glue_stack_name}"
 aws cloudformation delete-stack --stack-name ${glue_stack_name} > /dev/null
 
 echo "deleting ${syd_stack_name}"
-aws cloudformation delete-stack --stack-name "${syd_stack_name}-syd" > /dev/null
+aws cloudformation delete-stack --stack-name "${syd_stack_name}" > /dev/null
 
 # not recommended but will use this for now
 echo "deleting s3://${S3_BUCKET_NAME}"
