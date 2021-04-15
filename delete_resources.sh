76b47901b6d3faefdec9d6b7fbab020e58e73fc4
Baichuan Sun <sunbc0120@users.noreply.github.com>
2021-04-15 16:16:16 +1000

    Revert "Independent Test E2E"

--- delete_resources.sh
+++ delete_resources.sh
@@ -2,21 +2,14 @@
 
 athena_stack_name="athena_name"
 glue_stack_name="glue_name"
-syd_stack_name="syd_name"
+sdy_stack_name="sdy_name"
 S3_BUCKET_NAME="your_s3_bucket_name"
 
-echo "deleting ${athena_stack_name}"
-aws cloudformation delete-stack --stack-name ${athena_stack_name} > /dev/null
-
-echo "deleting ${glue_stack_name}"
-aws cloudformation delete-stack --stack-name ${glue_stack_name} > /dev/null
-
-echo "deleting ${syd_stack_name}"
-aws cloudformation delete-stack --stack-name "${syd_stack_name}" > /dev/null
+aws cloudformation delete-stack --stack-name ${athena_stack_name}
+ 
+aws cloudformation delete-stack --stack-name ${glue_stack_name}
+ 
+aws cloudformation delete-stack --stack-name "${sdy_stack_name}-sdy"
 
 # not recommended but will use this for now
-echo "deleting s3://${S3_BUCKET_NAME}"
-aws s3 rb s3://${S3_BUCKET_NAME} --force > /dev/null
-
-echo "restore delete_resources.sh"
-git checkout -- delete_resources.sh
+aws s3 rb s3://${S3_BUCKET_NAME} --force
