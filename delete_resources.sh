b373c9a5691b7d8bc9f0535a81908791be90f313
Baichuan Sun <baichsun@amazon.com>
2021-04-15 15:06:42 +1000

    ✏️ fix typos

--- delete_resources.sh
+++ delete_resources.sh
@@ -2,14 +2,18 @@
 
 athena_stack_name="athena_name"
 glue_stack_name="glue_name"
-sdy_stack_name="sdy_name"
+syd_stack_name="syd_name"
 S3_BUCKET_NAME="your_s3_bucket_name"
 
-aws cloudformation delete-stack --stack-name ${athena_stack_name}
- 
-aws cloudformation delete-stack --stack-name ${glue_stack_name}
- 
-aws cloudformation delete-stack --stack-name "${sdy_stack_name}-sdy"
+echo "deleting ${athena_stack_name}"
+aws cloudformation delete-stack --stack-name ${athena_stack_name} > /dev/null
+
+echo "deleting ${athena_stack_name}"
+aws cloudformation delete-stack --stack-name ${glue_stack_name} > /dev/null
+
+echo "deleting ${athena_stack_name}"
+aws cloudformation delete-stack --stack-name "${syd_stack_name}-syd" > /dev/null
 
 # not recommended but will use this for now
-aws s3 rb s3://${S3_BUCKET_NAME} --force
+echo "deleting s3://${S3_BUCKET_NAME}"
+aws s3 rb s3://${S3_BUCKET_NAME} --force > /dev/null
